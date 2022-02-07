8036f7e3f79f51e6da4ef3f083e0be259aa5d611
IgorSouza <igorsouzatb@hotmail.com>
2022-02-07 18:24:02 -0300

    adicionando metodos

---
+++ src/main/java/com/igor/championscrud/model/Champions.java
@@ -0,0 +1,74 @@
+package com.igor.championscrud.model;
+
+import java.util.Objects;
+
+public class Champions {
+    private Long id;
+    private String nome;
+    private String damage;
+    private String tier;
+    private Role role;
+
+    public Champions(){
+        super();
+    }
+    public Champions(Long id, String nome, String damage, String tier, Role role) {
+        this.id = id;
+        this.nome = nome;
+        this.damage = damage;
+        this.tier = tier;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDamage() {
+        return damage;
+    }
+
+    public void setDamage(String damage) {
+        this.damage = damage;
+    }
+
+    public String getTier() {
+        return tier;
+    }
+
+    public void setTier(String tier) {
+        this.tier = tier;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Champions champions)) return false;
+        return Objects.equals(id, champions.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/igor/championscrud/model/Role.java
@@ -0,0 +1,53 @@
+package com.igor.championscrud.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Role {
+    private Long id;
+    private String nome;
+
+
+    private  List<Champions> champions = new ArrayList<>();
+
+    public Role() {
+        super();
+    }
+
+    public Role(Long id, String nome) {
+        super();
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role role)) return false;
+        return Objects.equals(id, role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
+
+
