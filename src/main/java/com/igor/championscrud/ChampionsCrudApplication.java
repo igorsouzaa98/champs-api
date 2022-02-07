18b2ba0e0bbe70b79080d9f4534ead6e5e50e189
IgorSouza <igorsouzatb@hotmail.com>
2022-02-07 17:58:57 -0300

    Inciando o projeto de api

---
+++ src/main/java/com/igor/championscrud/ChampionsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.igor.championscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionsCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChampionsCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/igor/championscrud/ChampionsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.championscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionsCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
